d0ec196b00cab6e55c5d4634a2ff449665255674
nocturnalnerds <133097737+nocturnalnerds@users.noreply.github.com>
2024-06-15 20:50:32 +0700

    statistics

--- server/src/functions/statistics/createStatistics.ts
+++ server/src/functions/statistics/createStatistics.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from "express";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Topics } from "@prisma/client";
 import { AppError } from "../../utils/AppError";
 
 const prisma = new PrismaClient();
@@ -13,30 +13,87 @@ totalComplaints
 commonComplaints
 
 */
-
-export const createStatistics: RequestHandler = async (request, response, next) => {
-  try {
-    // userId
+// userId
     // totalComplaints = Jumlah responseQuery
     // commonComplaints = bruteforce aja
 
-    const {userId, totalComplaints, commonComplaints} = request.body;
-
-    if(!userId) throw new AppError("userId required", 402)
+export const createStatistics: RequestHandler = async (request, response, next) => {
+  try {
+    const userId = request.body.userId;
+    // const totalComplaints ;
+    // const commonComplaints;
+    if(!userId) throw new AppError("userId required", 401)
 
     const all_companies = await prisma.company.findMany({
         where : {userId}
     });
+    
     const all_request = await Promise.allSettled(all_companies.map(async(company) => {
         return prisma.requestQuery.findMany({
             where: {companyId: company.id}
         });
     }));
+
+    const arrayReq = all_request.map((request) =>{
+        if (request.status !== "fulfilled") return;
+        return request.value;
+    }).flat();
+
+    const totalComplaints = arrayReq.length;
+    
+    const all_response = await Promise.allSettled(all_companies.map(async(company) => {
+        return prisma.responseQuery.findMany({
+            where: {companyId: company.id}
+        });
+    }));
+    const arrayRes = all_response.map((request) =>{
+        if(request.status !== "fulfilled") return;
+        return request.value;
+    }).flat();
+
+    let commonComplaints = "";
+    const topicCounts = {
+        Billing: 0,
+        Products: 0,
+        Complaints: 0,
+        Services: 0,
+        Suggestions: 0,
+        Technical: 0
+    };
+    arrayRes.forEach(element => {
+        if (element?.topic && topicCounts.hasOwnProperty(element.topic)) {
+            topicCounts[element.topic]++;
+        }
+    });
+
+    let maxCount = 0;
+    for (const [topic, count] of Object.entries(topicCounts)) {
+        if (count > maxCount) {
+            maxCount = count;
+            commonComplaints = topic;
+        }
+    }
+
+    /*
+      topic     Topics
+totalComplaints
+commonComplaints
+    */
+    const newStatistic = await prisma.statistics.create({
+        data:{
+            userId,
+            totalComplaints: totalComplaints,
+            commonComplaints: commonComplaints as Topics,
+        },
+    });
+
     response.status(200).send({
         status: "success",
-        data: all_request,
+        data: newStatistic,
+        
     });
     
+
 
   } catch (error) {
     next(error);
